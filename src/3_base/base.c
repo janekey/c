44c4ddbb56c368874f1a0d9d3155c06c66d98e5a
janekey <zheng.q31@gmail.com>
2014-07-03 16:39:42 +0800

    base input output

--- src/3_base/base.c
+++ src/3_base/base.c
@@ -2,7 +2,7 @@
  * Date: 2014年7月2日
  * Author: Administrator
  */
-#include <stdio.h>
+#include <stdio.h>		//如果是"stdio.h"，编译系统现在当前目录寻找文件，若找不到再按标准模式  或"c:\temp\a.h"
 
 #define PI 3.1416		//符号常量（注意行末没有分号）：符号常量不占内存，预编译后直接置换成后面的值，符号就不存在了
 const int a = 3;		//常变量：是一种变量，值不能改变*C99
@@ -26,13 +26,8 @@ const int a = 3;		//常变量：是一种变量，值不能改变*C99
  * float_complex,double_complex,long long_complex
  */
 
-int main()
+int main31()
 {
-	/**
-	 * %u 无符号十进制
-	 * %d 十进制
-	 * %c 字符
-	 */
 	//各编译系统不同，基本类型占用字节略有不同
 	printf("short(%d)<=int(%d)<=long(%d)<=long long(%d)\n",
 			sizeof(short),sizeof(int),sizeof(long),sizeof(long long));
---
+++ src/3_base/base_inout.c
@@ -0,0 +1,33 @@
+/*
+ * base_inout.c
+ *
+ *  Created on: 2014年7月3日
+ *      Author: p_qizheng
+ */
+#include <stdio.h>
+
+int main()
+{
+	/**
+	 * %u 无符号十进制
+	 * %d 十进制	%5d 占5列		%ld 长整形	%lld 双长整形
+	 * %c 字符	%5c 占5列
+	 * %s 字符串	%5s 占5列
+	 * %f 实数	%m.nf 数据m列，小数n列		%-m.nf 与上面一样，向左对齐	%lf 双精度
+	 * %e 指数 小数6位，指数5列	%m.ne
+	 * %i 与%d相同
+	 * %o 八进制整数
+	 * %x 十六进制整数
+	 */
+	int a;
+	double b;
+	long c = 1l;
+	scanf("%d%lf%ld", &a, &b, &c);
+	printf("%d,%lf,%ld\n", a, b, c);
+
+	printf("%5d,%7.2f,%5s，%e,%o,%x\n",43,43.343,"dd",4343.44,44,44);
+
+	char x = getchar();
+	putchar(x);
+	return 0;
+}
